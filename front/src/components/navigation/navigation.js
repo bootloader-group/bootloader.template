db21caa24924f2d9032803b11ece1639be7a7f13
Marcin Maciejewski <m.maciejewski@nowaera.pl>
2020-06-08 10:47:09 +0200

    Add navigation links

--- front/src/components/navigation/navigation.js
+++ front/src/components/navigation/navigation.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <header>
+            <div className="container">
+                <ul>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </ul>
+            </div>
+        </header>
     )
 };
 
