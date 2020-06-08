5fb407b4f1896de8c58f4539083270547b358d43
Marcin Maciejewski <m.maciejewski@nowaera.pl>
2020-06-08 11:24:41 +0200

    [wip] Add about component

--- front/src/pages/about.js
+++ front/src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Wrapper from '../components/wrapper/wrapper';
+import About from '../components/about/about';
 
 export default function AboutPage() {
     return (
         <Wrapper>
             <div>About Page</div>
+            <About></About>
         </Wrapper>
     );
 }
