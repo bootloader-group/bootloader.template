cd6a576504be16eb97f503c1c814d644b598c6a6
Szymon <szymon.samolnik@gmail.com>
2020-08-11 15:04:05 +0200

    Update deploy.template

--- config/deploy.template.sh
+++ config/deploy.template.sh
@@ -3,5 +3,12 @@ cd /var/www/<<<ENV_NAME>>>>/<<<PROJECT_NAME>>>/
 git pull
 composer install
 composer update
+cd /var/www/<<<ENV_NAME>>>>/<<<PROJECT_NAME>>>/config/
 ./install.sh
 ./notify.sh
+cd /var/www/<<<ENV_NAME>>>>/<<<PROJECT_NAME>>>/web/app/themes/bootloader-theme/react-src/
+npm ci
+npm run wpbuild
+cd /var/www/<<<ENV_NAME>>>>/<<<PROJECT_NAME>>>/web/app/themes/bootloader-theme/
+cp -r bootloader-theme/* ./
+
